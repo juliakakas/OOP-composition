b3dfff95a091c46bf26a5c5a0193a83bfbb04141
Lia <julia.kakas@gmail.com>
2023-07-02 07:27:33 +0200

    1.0

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,38 @@
 public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name,String email, char gender){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+
+    public String toString() {
+        return super.toString();
+    }
 }
